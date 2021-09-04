1b769429a10e6f23dfaca83edbc464e5e4198b80
Yuko <yuko.kumason@gmail.com>
2021-09-03 19:12:15 -0700

    drawMiddleBorder added

--- box_it_script/homework1.js
+++ box_it_script/homework1.js
@@ -14,10 +14,22 @@ function drawTopBorder(number) {
     let firstCornerLine = "\u250F";
     let secondCornerLine = "\u2513";
     newLine = drawLine(4)
-    // for (let j = 1; j <= number; j++) {
+    // for (let j = 1; j <= number; j++) 
     //     newLine += "\u2501"
     // }
     return firstCornerLine + newLine + secondCornerLine;
 
 }
 console.log(drawTopBorder(0));
+
+
+
+
+function drawMiddleBorder(number) {
+    let newLine = ""
+    let firstVerLine = "\u2523";
+    let secondVerLine = "\u252B"
+    newLine = drawLine(4)
+    return firstVerLine + newLine + secondVerLine;
+}
+console.log(drawMiddleBorder(8));
