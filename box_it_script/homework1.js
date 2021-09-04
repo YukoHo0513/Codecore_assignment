1e7c8ad9901d8182540abe971e68dee12af49250
Yuko <yuko.kumason@gmail.com>
2021-09-03 18:38:32 -0700

    drawTopBorder functionadded

--- box_it_script/homework1.js
+++ box_it_script/homework1.js
@@ -6,3 +6,18 @@ function drawLine(number) {
     return newLine;
 }
 console.log(drawLine(4));
+
+
+
+function drawTopBorder(number) {
+    let newLine = "";
+    let firstCornerLine = "\u250F";
+    let secondCornerLine = "\u2513";
+    newLine = drawLine(4)
+    // for (let j = 1; j <= number; j++) {
+    //     newLine += "\u2501"
+    // }
+    return firstCornerLine + newLine + secondCornerLine;
+
+}
+console.log(drawTopBorder(0));
