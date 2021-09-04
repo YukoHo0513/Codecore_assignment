d9a3cb5bfe99b8f9ef3e714e0592eac9d21c9968
Yuko <yuko.kumason@gmail.com>
2021-09-04 14:06:06 -0700

    boxIt function added

--- box_it_script/homework1.js
+++ box_it_script/homework1.js
@@ -11,10 +11,9 @@ console.log(drawLine(8));
 
 
 function drawTopBorder(number) {
-    let newLine = "";
     let firstCornerLine = "\u250F";
     let secondCornerLine = "\u2513";
-    newLine = drawLine(number)
+    let newLine = drawLine(number)
     // for (let j = 1; j <= number; j++) 
     //     newLine += "\u2501"
     // }
@@ -27,10 +26,9 @@ console.log(drawTopBorder(0));
 
 
 function drawMiddleBorder(number) {
-    let newLine = ""
     let firstVerLine = "\u2523";
     let secondVerLine = "\u252B"
-    newLine = drawLine(number)
+    let newLine = drawLine(number)
     return firstVerLine + newLine + secondVerLine;
 }
 console.log(drawMiddleBorder(8));
@@ -39,10 +37,9 @@ console.log(drawMiddleBorder(0));
 
 
 function drawBottomBorder(number) {
-    let newLine = "";
     let firstCornerLine = "\u2517";
     let secondCornerLine = "\u251B"
-    newLine = drawLine(number);
+    let newLine = drawLine(number);
     return firstCornerLine + newLine + secondCornerLine;
 }
 console.log(drawBottomBorder(2));
@@ -57,3 +54,38 @@ function drawBarsAround(string){
 console.log(drawBarsAround("My name is Dan"));
 console.log(drawBarsAround("You are Jane  "));
 console.log(drawBarsAround("  You are Bill"));
+
+
+
+
+
+function boxIt(array) {
+    let longestLength = array[0].length;
+    for (let x = 1; x < array.length; x++) {
+        if (array[x].length > longestLength) {
+            longestLength = array[x].length;
+        }
+    }
+    let result = "";
+    if (array.length === 1) {
+        let x = 0;
+        result += drawTopBorder(longestLength) + "\n" + drawBarsAround(array[x]) + "\n" 
+        + drawBottomBorder(longestLength);
+    } else {
+        for (let x = 0; x < array.length; x++){
+            if (x === array.length -1) {
+                result += "\n" + drawBarsAround(array[x]) + "\n" + drawBottomBorder(longestLength);
+            } else if (x === 0) {
+                result += drawTopBorder(longestLength) + "\n" + drawBarsAround(array[x]) + "\n" 
+                + drawMiddleBorder(longestLength);
+            } else {
+                result += "\n" + drawBarsAround(array[x]) + "\n" + drawMiddleBorder(longestLength);
+            }
+    
+    }
+}
+    return result
+}
+
+console.log(boxIt(['Jon Snow', 'Cersei Lannister', 'Yuko Ho']));
+console.log(boxIt(['Jon Snow']));
