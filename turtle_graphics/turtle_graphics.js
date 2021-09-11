5884264570033e54fca1605a5166c7c1aa0124f5
Yuko <yuko.kumason@gmail.com>
2021-09-10 20:37:52 -0700

    initial_commit_hw_2

---
+++ turtle_graphics/turtle_graphics.js
@@ -0,0 +1,27 @@
+class Turtle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.path = [];
+    }
+    forward(numOfSteps) {
+        
+    }
+    right() {
+
+    }
+    left() {
+
+    }
+    allPoints() {
+
+    }
+    print() {
+
+    }
+}
+const newTurtle = new Turtle(0, 0)
+console.log(newTurtle);
+
+// x is yoko
+// y is tate
