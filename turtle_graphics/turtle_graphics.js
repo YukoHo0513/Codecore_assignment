54492181957c719bbbd88dd3c10b25a8ba27169c
Yuko <yuko.kumason@gmail.com>
2021-09-16 20:57:30 -0700

    finisished the whole class

--- turtle_graphics/turtle_graphics.js
+++ turtle_graphics/turtle_graphics.js
@@ -1,46 +1,53 @@
 class Turtle {
-    constructor(x, y){
+    constructor(x, y) {
         this.x = x;
         this.y = y;
         this.direction = 0;
 
         if (this.x === undefined) {
             this.x = 0;
-        } 
+        }
         if (this.y === undefined) {
             this.y = 0;
         }
         this.bigPath = [[this.x, this.y]];
     }
-    forward(numOfStep){
-        this.path = [];
+    forward(numOfStep) {
 
         if (this.direction === 360 || this.direction === 0) { // facing east
             for (let i = 0; i < numOfStep; i++) {
-                this.x += numOfStep;
+                this.path = [];
+                this.x += 1;
                 this.path.push(this.x);
                 this.path.push(this.y);
                 this.bigPath.push(this.path);
             }
-            this.x += numOfStep;
-            this.path.push(this.x);
-            this.path.push(this.y);
-            this.bigPath.push(this.path);
+
         } else if (this.direction === 90) { // facing south
-            this.y += numOfStep;
-            this.path.push(this.x);
-            this.path.push(this.y);
-            this.bigPath.push(this.path);
+            for (let i = 0; i < numOfStep; i++) {
+                this.path = [];
+                this.y += 1;
+                this.path.push(this.x);
+                this.path.push(this.y);
+                this.bigPath.push(this.path);
+            }
+
         } else if (this.direction === 180) { // facing west
-            this.x -= numOfStep;
-            this.path.push(this.x);
-            this.path.push(this.y);
-            this.bigPath.push(this.path);
+            for (let i = 0; i < numOfStep; i++) {
+                this.path = [];
+                this.x -= 1;
+                this.path.push(this.x);
+                this.path.push(this.y);
+                this.bigPath.push(this.path);
+            }
         } else if (this.direction === 270) { // facing north
-            this.y -= numOfStep;
-            this.path.push(this.x);
-            this.path.push(this.y);
-            this.bigPath.push(this.path);
+            for (let i = 0; i < numOfStep; i++) {
+                this.path = [];
+                this.y -= 1;
+                this.path.push(this.x);
+                this.path.push(this.y);
+                this.bigPath.push(this.path);
+            }
         }
 
         return this.bigPath;
@@ -59,39 +66,78 @@ class Turtle {
         this.direction -= 90;
     }
     allPoints() {
-        this.bigPath;
+        return this.bigPath;
     }
     print() {
-        for (let i = 0; i < 5; i++) {
-            this.row1 = ["□", "□", "□", "□", "□"];
-            
+
+
+        this.maxX = this.bigPath[0][0];
+        for (let i = 0; i < this.bigPath.length; i++) {
+
+            if (this.maxX < this.bigPath[i][0]) {
+                this.maxX = this.bigPath[i][0]
+            }
         }
-        return this.row1;
+        this.minX = this.bigPath[0][0];
+        for (let i = 0; i < this.bigPath.length; i++) {
+            if (this.minX > this.bigPath[i][0]) {
+                this.minX = this.bigPath[i][0]
+            }
+        }
+        if (this.minX < 0) {
+            this.minX = Math.abs(this.minX);
+            this.maxX += this.minX;
+            for (let i = 0; i < this.bigPath.length; i++) {
+                this.bigPath[i][0] += this.minX
+            }
+        }
+
+        this.maxY = this.bigPath[0][1];
+        for (let i = 0; i < this.bigPath.length; i++) {
+
+            if (this.maxY < this.bigPath[i][1]) {
+                this.maxY = this.bigPath[i][1]
+            }
+        }
+
+        this.minY = this.bigPath[0][1];
+        for (let i = 0; i < this.bigPath.length; i++) {
+            if (this.minY > this.bigPath[i][1]) {
+                this.minY = this.bigPath[i][1]
+            }
+        }
+
+        if (this.minY < 0) {
+            this.minY = Math.abs(this.minY)
+            this.maxY += this.minY;
+            for (let i = 0; i < this.bigPath.length; i++) {
+                this.bigPath[i][1] += this.minY;
+            }
+        }
+
+        this.result = [];
+        for (let i = 0; i <= this.maxY; i++) {
+            this.result.push(Array.from({ length: this.maxX + 1 }).fill('□'))
+
+        }
+
+        for (let i = 0; i < this.bigPath.length; i++) {
+            this.result[this.bigPath[i][1]][this.bigPath[i][0]] = '■'
+        }
+
+        for (let row of this.result) {
+            console.log(row)
+        }
+
     }
 }
 const Yuko = new Turtle()
-console.log(Yuko);
-// console.log(Yuko.forward(3));
 Yuko.forward(3);
-console.log(Yuko.bigPath);
+Yuko.right();
+Yuko.forward(1);
+Yuko.left();
+Yuko.left();
 Yuko.forward(2);
-console.log(Yuko.bigPath);
+Yuko.left();
+Yuko.forward(4);
 Yuko.print();
-console.log(this.row1);
-
-
-// x is yoko
-// y is tate
-
-// Remember that first position starts from East which is 90 degree.
-// 1) Right() => 180 south
-// 2) Right() => 270 west
-// 3) Right() => 360 north
-// 4) Right() => 90 east
-
-// 1) Left() => 360 north
-// 2) Left() => 270 west
-// 3) Left() => 180 south
-// 4) Left() => 90 east
-
-// 0 or 360 faces to North
